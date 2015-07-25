9a27457e31d53d9804b609b6bee196e1fa5365e5
Robert Collins <rbtcollins@hp.com>
2015-07-26 06:50:51 +1200

    Issue #24710: Use cls in TracebackException.from_exception.
    Minor cleanup patch from Berker Peksag.

--- traceback2/__init__.py
+++ traceback2/__init__.py
@@ -489,13 +489,12 @@ class TracebackException:
             self._load_lines()
 
     @classmethod
-    def from_exception(self, exc, *args, **kwargs):
+    def from_exception(cls, exc, *args, **kwargs):
         """Create a TracebackException from an exception.
         
         Only useful in Python 3 specific code.
         """
-        return TracebackException(
-            type(exc), exc, exc.__traceback__, *args, **kwargs)
+        return cls(type(exc), exc, exc.__traceback__, *args, **kwargs)
 
     def _load_lines(self):
         """Private API. force all lines in the stack to be loaded."""
